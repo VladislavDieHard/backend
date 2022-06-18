d2dbf29bcecbf9c7eb1082ca5c8c9fb7cecb4ee0
DieHard <Linerace2010>
2022-06-18 13:58:33 +0700

    update slugify util

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,11 @@
 import slugify from 'slugify';
 import moment from 'moment';
 
-export function createSlug(title: string, slug: string | undefined) {
+export function createSlug(
+  title: string,
+  slug: string | undefined,
+  slugWithDate = true,
+) {
   let resultedSlug;
 
   if (slug) {
@@ -17,7 +21,9 @@ export function createSlug(title: string, slug: string | undefined) {
     });
   }
 
-  resultedSlug += `-${moment().format('HH:mm-DD.MM.YY')}`;
+  if (slugWithDate) {
+    resultedSlug += `-${moment().format('HH:mm-DD.MM.YY')}`;
+  }
 
   return resultedSlug;
 }
