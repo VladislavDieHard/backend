956487d446c617808f26f5e5c2e6d588b920b12e
nixel <Fktrctq492{hjvjd492>
2022-08-12 11:56:58 +0700

    add get files

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from "@nestjs/common";
 import { FileService } from './file.service';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('File')
 @Controller('file')
 export class FileController extends FileService {
+  @Get()
+  getFiles(
+    @Query('pageSize') pageSize:number,
+    @Query('page') page:number,
+    @Query('searchByField') searchByField?: string,
+  ) {
+    return this.fileGetService.getFiles({
+      pageSize,
+      page,
+      searchByField
+    })
+  }
+
   @Get(':idOrSlug')
-  getEntry(@Param('idOrSlug') idOrSlug: number | string) {
+  getFile(@Param('idOrSlug') idOrSlug: number | string) {
     return this.fileGetService.getFile(idOrSlug);
   }
 }
